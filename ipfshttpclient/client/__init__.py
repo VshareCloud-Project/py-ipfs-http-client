98207a1ca537462f780944860f0d3a2a478aad70
Yang <76504564+yry0008@users.noreply.github.com>
2022-04-01 11:40:23 +0800

    Disable maxium version warning

--- ipfshttpclient/client/__init__.py
+++ ipfshttpclient/client/__init__.py
@@ -71,7 +71,7 @@ def assert_version(version: str, minimum: str = VERSION_MINIMUM,
 	minimum = list(map(int, minimum.split('-', 1)[0].split('.')))
 	maximum = list(map(int, maximum.split('-', 1)[0].split('.')))
 
-	if minimum > version or version >= maximum:
+	if minimum > version:
 		warnings.warn(exceptions.VersionMismatch(version, minimum, maximum))
 
 	for blacklisted in blacklist:
